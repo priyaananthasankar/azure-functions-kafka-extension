e9b0ea885ca364b3989687a76f7b7424a2626c18
Priya Ananthasankar <prananth@microsoft.com>
2019-05-02 19:23:09 -0400

    install package changes

--- binding-library/python/setup.py
+++ binding-library/python/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='azure-functions-kafka-binding',
-    version='1.0.0',
-    packages=['azure_functions.kafka']
+    version='1.0.2',
+    packages=['azure_functions.kafka'],
+    install_requires=['azure-functions-worker']
 )
